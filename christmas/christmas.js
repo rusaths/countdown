8ff40552b5c6653e59895f962177f86d23240515
rusaths <rusaaa2008@gmail.com>
2022-12-28 22:06:17 -0800

    updated

---
+++ christmas/christmas.js
@@ -0,0 +1,27 @@
+const newYear = "25 Dec 2023";
+
+function countdown(){
+    const newYearDate = new Date(newYear);
+    const  currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate)/1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours  = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60 ) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    document.getElementById("days").innerHTML = timeformat(days);
+    document.getElementById("hours").innerHTML = timeformat(hours);
+    document.getElementById("minutes").innerHTML = timeformat(minutes);
+    document.getElementById("seconds").innerHTML = timeformat(seconds);
+
+}
+
+function timeformat(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+if(countdown)
+
+setInterval(countdown, 1000);
---
+++ newyear/new year.js
@@ -0,0 +1,25 @@
+const newYear = "01 Jan 2023";
+
+function countdown(){
+    const newYearDate = new Date(newYear);
+    const  currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate)/1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours  = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60 ) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    document.getElementById("days").innerHTML = timeformat(days);
+    document.getElementById("hours").innerHTML = timeformat(hours);
+    document.getElementById("minutes").innerHTML = timeformat(minutes);
+    document.getElementById("seconds").innerHTML = timeformat(seconds);
+
+}
+
+function timeformat(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(countdown, 1000);
